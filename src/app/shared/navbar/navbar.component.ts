f2c410e68e806d14d72f0ca3eb961e703f86d0b7
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-11-25 17:39:08 -0500

    ui routerLink

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  goUser(id: string) {
+    if (!id) return;
+    this.router.navigate(['/user', id]);
+  }
 }
